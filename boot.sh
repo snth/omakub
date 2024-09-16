512bdd02aa7b0d2231b0a63abb17b0ccb2576ca9
Tobias Brandt <Tobias.Brandt@gmail.com>
2024-09-16 22:53:27 +0200

    Switches boot.sh to point to my repo and branch

--- boot.sh
+++ boot.sh
@@ -17,11 +17,12 @@ sudo apt-get install -y git >/dev/null
 
 echo "Cloning Omakub..."
 rm -rf ~/.local/share/omakub
-git clone https://github.com/basecamp/omakub.git ~/.local/share/omakub >/dev/null
+git clone https://github.com/snth/omakub.git ~/.local/share/omakub >/dev/null
 if [[ $OMAKUB_REF != "master" ]]; then
-	cd ~/.local/share/omakub
-	git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
-	cd -
+  cd ~/.local/share/omakub
+  git fetch origin "${OMAKUB_REF:-main}" && git checkout "${OMAKUB_REF:-main}"
+  echo "Installing snth/omakub/$(git branch --show)"
+  cd -
 fi
 
 echo "Installation starting..."
