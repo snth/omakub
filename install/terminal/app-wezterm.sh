1a593e4a7b5b543c0e9d0ca9dfff9e8471f06c8f
Tobias Brandt <Tobias.Brandt@gmail.com>
2024-09-17 10:02:03 +0200

    Adds wezterm for ssh_domain multiplexing

---
+++ install/terminal/app-wezterm.sh
@@ -0,0 +1,7 @@
+# Add the wezterm keys and repo
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+
+# Install wezterm
+sudo apt update
+sudo apt install wezterm
