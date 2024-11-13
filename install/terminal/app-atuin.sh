39f92bacf4b6fdad8b6dd0af28a6eb1dbe4774ef
Tobias Brandt <Tobias.Brandt@gmail.com>
2024-11-13 14:32:37 +0200

    Adds atuin shell history

---
+++ install/terminal/app-atuin.sh
@@ -0,0 +1,14 @@
+# install
+curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+
+# set up sync
+#atuin register -u <YOUR_USERNAME> -e <YOUR EMAIL>
+
+# check the key
+#atuin key
+
+# syncing
+#atuin sync
+
+# login
+#atuin login -u <USERNAME>
