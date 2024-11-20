ad410cac9735234a2122058edab6cc8aa35b2455
Tobias Brandt <Tobias.Brandt@gmail.com>
2024-11-20 23:28:51 +0200

    Adds ble.sh

---
+++ install/terminal/app-ble.sh
@@ -0,0 +1,3 @@
+wget -O - https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf -
+bash ble-nightly/ble.sh --install ~/.local/share
+echo 'source ~/.local/share/blesh/ble.sh' >>~/.bashrc
