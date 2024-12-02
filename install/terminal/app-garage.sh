1d30b6179e6f7722a30fc8f42a578f43d1e57997
Tobias Brandt <Tobias.Brandt@gmail.com>
2024-12-02 21:42:32 +0200

    Adds S3 Garage

---
+++ install/terminal/app-garage.sh
@@ -0,0 +1,3 @@
+VERSION=v1.0.1
+#curl -o ~/.local/bin/garage https://garagehq.deuxfleurs.fr/_releases/${VERSION}/x86_64-unknown-linux-musl/garage
+wget -o ~/.local/bin/garage --show-progress https://garagehq.deuxfleurs.fr/_releases/${VERSION}/x86_64-unknown-linux-musl/garage
